f126862426be3c4af3671e91a853935114e85ffa
Streamlits321 <stat38399@gmail.com>
2025-03-05 12:22:28 +0200

    Update Web.py

--- Web.py
+++ Web.py
@@ -9,7 +9,14 @@ import pandas as pd
 import io
 
 st.set_page_config(page_title="My App")
-
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
 
 st.markdown(
     """
